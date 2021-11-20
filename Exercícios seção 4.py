89ba4631b873b58fc4ef21ea7f4bd813ba4282bb
Rafael Ramos <93404382+Rafarxc@users.noreply.github.com>
2021-11-19 22:56:40 -0300

    Add files via upload

---
+++ Exercícios seção 4.py
@@ -0,0 +1,23 @@
+"""
+Exercício 8
+TempKelvin = int(input('Temperatura em Kelvin é: '))
+TempCelsius = TempKelvin - 273.15
+print(f'Temperatura em ºC é igual a {TempCelsius} graus')
+
+-------------------------------------------------------------
+print(f'Em minutos:{segundos // 60} min')
+print(f'Em horas: {segundos // 3600} horas')
+print(f'Em segundos {segundos} seg')
+
+segundos = int(input('Quantos segundos o senhor deseja ?'))
+
+print(f'{int(segundos / 3600)} h {int(segundos % 3600 / 60)} min e {(segundos % 3600) % 60 } segundos')
+
+------------------------------------------------------------------------
+"""
+
+
+dados = input("Digite três numeros: ")
+print(dados[2] + dados[1] + dados[0])
+
+
